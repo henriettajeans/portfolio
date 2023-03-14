500827743c104fa0077000a802b1b77f64387bef
henrietta jeansson <henriettajeansson@gmail.com>
2023-03-14 19:53:17 +0100

    fonts in about page

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import { VscGithubInverted } from "react-icons/vsc";
+import './about.scss';
 
-export const About =()=>{
-    return(
+export const About = () => {
+    return (
         <><h1>A little bit about me</h1>
-        <p>A link to my resume ___</p>
-        <p>My linkedin profile <Link to="https://www.linkedin.com/in/henrietta-jeansson/">right here</Link></p>
-        <p> Check out my <VscGithubInverted/></p>
+            <p>A link to my resume ___</p>
+            <p>My linkedin profile <Link to="https://www.linkedin.com/in/henrietta-jeansson/">right here</Link></p>
+            <p> Check out my <VscGithubInverted /></p>
         </>
     )
 }
